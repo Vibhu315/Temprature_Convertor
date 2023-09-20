3d5ce19f5f717e662cbc48c779d4074084fee219
Vibhu Patel <93249125+Vibhu315@users.noreply.github.com>
2023-09-20 12:55:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 let c = document.getElementById("Celsius");
 let f = document.getElementById("Fahrenheit");
 let k = document.getElementById("Kelvin");
-function convert(){
-    let cinput = parseFloat(c.value);
-    let  finput=(cinput * 9 / 5) + 32;
-    let kinput=cinput + 273.15;
-        f.value=finput;
-        k.value=kinput;
 
+ c.addEventListener("input",function(){
+    fer.value=parseFloat((c.value)*9/5)+32;
+    kel.value=parseFloat(c.value)+273.15;
+ })
+ f.addEventListener("input",function(){
+    cel.value=parseFloat((f.value-32)*5/9);
+    kel.value=parseFloat((f.value-32)*5/9+273.15);
+ })
+ k.addEventListener("input",function(){
+    fer.value=parseFloat((k.value-273.15)*9/5)+32;
+    cel.value=parseFloat(k.value)-273.15;
+ })
+
+function agian() {
+c.value = "";
+f.value = "";
+k.value = "";
 }
