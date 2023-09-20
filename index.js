71c7be1dd4d8e2dec64eafc1a8a1c002f679d4ce
Vibhu Patel <93249125+Vibhu315@users.noreply.github.com>
2023-09-20 13:00:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ let f = document.getElementById("Fahrenheit");
 let k = document.getElementById("Kelvin");
 
  c.addEventListener("input",function(){
-    fer.value=parseFloat((c.value)*9/5)+32;
-    kel.value=parseFloat(c.value)+273.15;
+    f.value=parseFloat((c.value)*9/5)+32;
+    k.value=parseFloat(c.value)+273.15;
  })
  f.addEventListener("input",function(){
-    cel.value=parseFloat((f.value-32)*5/9);
-    kel.value=parseFloat((f.value-32)*5/9+273.15);
+    c.value=parseFloat((f.value-32)*5/9);
+    k.value=parseFloat((f.value-32)*5/9+273.15);
  })
  k.addEventListener("input",function(){
-    fer.value=parseFloat((k.value-273.15)*9/5)+32;
-    cel.value=parseFloat(k.value)-273.15;
+    f.value=parseFloat((k.value-273.15)*9/5)+32;
+    c.value=parseFloat(k.value)-273.15;
  })
 
 function agian() {
